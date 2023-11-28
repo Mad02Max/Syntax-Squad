db75490c0c6e5624cac6237374c4135a04b7a8dc
Simon Ståhl <simon.stahl94@gmail.com>
2023-11-28 11:20:35 +0100

    Transfer class added

---
+++ Syntax Squad/Transfer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax_Squad
+{
+    public class Transfer : ExchangeRateManager 
+    {
+        
+
+    public static void ShowAccountBalance()
+        {
+            
+            {
+                Console.WriteLine($"{ [i]}: { [i]} {exchangeRate[currencyCode]}");
+            }
+        }
+
+        public static void WithdrawFromAccount()
+        {
+
+        }
+
+        public static void TransferBetweenOwnAccounts()
+        {
+
+        }
+
+        public static void TransferBetweenOtherAccounts()
+        {
+
+        }
+
+    }
+}
