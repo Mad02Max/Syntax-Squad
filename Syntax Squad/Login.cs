6ab2c6139c417b4975bc0a08abeef89ba6d7c49c
Anton Larsson <anton.cj.sk8@gmail.com>
2023-11-30 13:37:37 +0100

    Login Class and method

---
+++ Syntax Squad/Login.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax_Squad
+{
+    public class Login : User
+    {
+        private const int MaxAttempts = 3;
+
+        public  void LogIn()
+        {          
+            List<User> allUsers = new List<User>();
+            allUsers.AddRange(RegularUser.GetRegular());
+            allUsers.AddRange(Admin.GetAdmins());
+
+            int attempts = 0;           
+
+            do
+            {
+                Console.WriteLine("Welcome to Syntax Squad Bank!");
+                Console.WriteLine("Username: ");
+                string enterUsername = Console.ReadLine();
+
+                Console.WriteLine("Password: ");
+                string enterPassword = Console.ReadLine();
+
+                User userTryLogin = allUsers.Find(x => x.Name == enterUsername && x.Password == enterPassword);
+
+                if (userTryLogin != null)
+                {
+                    Console.WriteLine("Login successful! press enter to continue to meny.");
+                    Console.ReadKey();
+
+                    if (userTryLogin.IsAdmin)
+                    {
+                        //AdminMenu();
+                    }
+                    else
+                    {
+                       //RegularUserMenu();
+                    }
+                    return;
+                }
+                else
+                {
+                    attempts++;
+                    Console.WriteLine($"Incorrect username or password. Attempts left: {MaxAttempts - attempts}");
+                }
+            }while ( attempts < MaxAttempts );
+            Console.WriteLine("Maximum login attempts reached. Account locked.");
+
+            
+            
+        }
+        public int GetUserID()
+        {
+            return UserId;
+        }
+    
+    }
+}
