2ce381ccfceb0654602c32bc96fe580760b1e9da
Simon Ståhl <simon.stahl94@gmail.com>
2023-11-30 14:31:31 +0100

    Transfer Class added. Password object not implemented into the code.

--- Syntax Squad/Transfer.cs
+++ Syntax Squad/Transfer.cs
@@ -6,32 +6,75 @@ using System.Threading.Tasks;
 
 namespace Syntax_Squad
 {
-    public class Transfer : ExchangeRateManager 
+    public class Transfer
     {
         
-
-    public static void ShowAccountBalance()
+        private List<BankAccount> transferAccounts = BankAccount.bankAccounts;
+        
+       
+        public void WithdrawFromAccount(int fromAccountNumber, double amount, string Userpassword)
         {
-            
+
+            var fromAccount = GetBankAccount(fromAccountNumber);
+            if (fromAccount.Balance == null || fromAccount.Balance < 0 && Userpassword == password)
             {
-                Console.WriteLine($"{ [i]}: { [i]} {exchangeRate[currencyCode]}");
+                Console.WriteLine("Insufficient fund on selected Account.");
+                return;
             }
+            fromAccount.Balance = amount;
+
+            Console.WriteLine($"Withdraw request successfull. Please take your money.");
+            Console.WriteLine($"Remaining Balance for {fromAccount}: {fromAccount.Balance}");
         }
 
-        public static void WithdrawFromAccount()
+        public void TransferBetweenOwnAccounts(int fromAccountNumber, int toAccountNumber, double amount, string Userpassword)
         {
+            var fromAccount = GetBankAccount(fromAccountNumber);
+            var toAccount = GetBankAccount(toAccountNumber);
+
+            if (fromAccount.Balance < amount )
+            {
+                Console.WriteLine("Insufficient funds.");
+                return;
+            }
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+
+            Console.WriteLine($"Transfer successful. New balance for {fromAccountNumber}: {fromAccount.Balance}");
+            Console.WriteLine($"New balance for {toAccountNumber}: {toAccount.Balance}");
 
         }
 
-        public static void TransferBetweenOwnAccounts()
+        public void TransferBetweenOtherAccounts(int fromAccountNumber, int toAccountNumber, double amount, string Userpassword)
         {
+            var fromAccount = GetBankAccount(fromAccountNumber);
+            var toAccount = GetBankAccount(toAccountNumber);
+            string password;
+
+            if (fromAccount == null || toAccount == null && password == Userpassword)
+            {
+                Console.WriteLine("Invalid account number.");
+                return;
+            }
+
+            if (fromAccount.Balance < amount)
+            {
+                Console.WriteLine("Insufficient funds.");
+                return;
+            }
+
+            fromAccount.Balance -= amount;
+            toAccount.Balance += amount;
+
+            Console.WriteLine($"Transfer successful. New balance for {fromAccountNumber}: {fromAccount.Balance:C}");
+            
 
         }
 
-        public static void TransferBetweenOtherAccounts()
+        public BankAccount GetBankAccount(int AccountNumber)
         {
-
+            return BankAccount.bankAccounts.Find(a => a.AccountNumber == AccountNumber);
         }
-
+               
     }
 }
