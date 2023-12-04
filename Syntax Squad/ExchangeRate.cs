c9b65e0b9164f47cb12b3fa008d9aa4089cf3fad
Simon Ståhl <simon.stahl94@gmail.com>
2023-12-04 11:16:58 +0100

    ChangeExchangeRate() lagt till så att man ändrar rate i metoden, istället för från menyn

--- Syntax Squad/ExchangeRate.cs
+++ Syntax Squad/ExchangeRate.cs
@@ -21,10 +21,16 @@ namespace Syntax_Squad
             };
         }
 
-        public void ChangeExchangeRates(string currencyCode, decimal newRate)
+        public void ChangeExchangeRates()
         {
+            DisplayExhangeRates();
+            Console.WriteLine("Enter currency code to change currency rate:");
+            string currencyCode = Console.ReadLine();
+
             if (exchangeRates.ContainsKey(currencyCode))
             {
+                Console.WriteLine("Enter new rate:");
+                decimal newRate = Convert.ToDecimal(Console.ReadLine());
                 exchangeRates[currencyCode] = newRate;
                 Console.WriteLine($"Exchange rate for {currencyCode} has been changed to {newRate}");
             }
